a316deeeb02accb2c7144b7fe4013c6f243b527c
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-17 14:16:14 -0400

    Probando conexion a MySQL

--- msvc-usuarios/src/main/java/org/atravieso/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
+++ msvc-usuarios/src/main/java/org/atravieso/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
@@ -5,7 +5,4 @@ import org.atravieso.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-
-
-
 }
--- msvc-usuarios/src/main/java/org/atravieso/springcloud/msvc/usuarios/services/UsuarioService.java
+++ msvc-usuarios/src/main/java/org/atravieso/springcloud/msvc/usuarios/services/UsuarioService.java
@@ -7,10 +7,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioService {
-
     List<Usuario> listar();
     Optional<Usuario> porId(Long id);
     Usuario guardar(Usuario usuario);
     void eliminar(Long id);
-
 }
