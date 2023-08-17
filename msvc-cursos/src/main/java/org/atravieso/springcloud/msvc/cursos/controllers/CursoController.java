d5f3109082668b8855b8da690cfd32a3512c09b9
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-17 15:28:44 -0400

    CRUD msvc cursos finalizado

---
+++ msvc-cursos/src/main/java/org/atravieso/springcloud/msvc/cursos/controllers/CursoController.java
@@ -0,0 +1,62 @@
+package org.atravieso.springcloud.msvc.cursos.controllers;
+
+import org.atravieso.springcloud.msvc.cursos.entity.Curso;
+import org.atravieso.springcloud.msvc.cursos.services.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CursoController {
+
+    @Autowired
+    private CursoService cursoService;
+
+    @GetMapping
+    public ResponseEntity<List<Curso>> listar() {
+        return ResponseEntity.ok(cursoService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
+        Optional<Curso> curso = cursoService.porId(id);
+        if(curso.isPresent()) {
+            return ResponseEntity.ok(curso.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Curso curso) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.guardar(curso));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+        Optional<Curso> cursoDB = cursoService.porId(id);
+        if(cursoDB.isPresent()) {
+            Curso cursoActualizado = cursoDB.get();
+            curso.setNombre(curso.getNombre());
+            return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.guardar(cursoActualizado));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Curso> cursoDB = cursoService.porId(id);
+        if(cursoDB.isPresent()) {
+            cursoService.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+}
